03e318ddbb93994e2202284ce5855e66d913b483
Mateusz Świetlicki <mateusz@swietlicki.net>
2020-05-14 23:24:38 +0200

    MS.Underfloor.Api

---
+++ src/MS.Underfloor.Api/Controllers/TempReport.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MS.Underfloor.Api.Controllers
+{
+    public class TempReport {
+        public double[] Temps { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ src/MS.Underfloor.Api/Controllers/TempsController.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MS.Underfloor.Api.Controllers
+{
+    [ApiController]
+    [Route("api/temps")]
+    public class TempsController : ControllerBase
+    {
+        public static List<TempReport> Reports { get; set; } = new List<TempReport>();
+        private readonly ILogger<TempsController> _logger;
+
+        public TempsController(ILogger<TempsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(TempReport report)
+        {
+            if(report == null)
+                return BadRequest();
+
+            Reports.Add(report);
+
+            return Accepted();
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            return Ok(Reports);
+        }
+    }
+}
